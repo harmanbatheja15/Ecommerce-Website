f054b1dd6e98a9c796dd555aa4ecedf67ea80430
harmanbatheja15 <harmanbatheja15@gmail.com>
2024-02-04 02:24:41 +0530

    Frontend Complete

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -5,9 +5,50 @@ import { Link, useNavigate } from "react-router-dom";
 const Products = () => {
 	const [products, setProducts] = useState([]);
 
+	const token = localStorage.getItem("token");
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		axios
+			.get("http://localhost:3000/products")
+			.then((response) => {
+				const data = response.data;
+				setProducts(data.products);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
+
 	return (
 		<>
-			<h1>Products</h1>
+			<div className="mx-auto max-w-7xl px-2">
+				<h1 className="text-3xl mt-9 mb-6">Products</h1>
+				{/* products */}
+				<div className="grid gap-6 gap-y-10 py-6 md:grid-cols-2 lg:grid-cols-3">
+					{products.map((product, index) => (
+						<div key={index} className="border">
+							<Link to={`/product/${product._id}`}>
+								<img
+									src={product.imageUrl}
+									className="w-full h-96 rounded-md"
+									alt={product.title}
+								/>
+							</Link>
+							<div className="min-h-min p-3">
+								<Link to={`/product/${product._id}`}>
+									<p className="mt-4 flex-1 text-base font-semibold text-gray-900 underline">
+										{product.title}
+									</p>
+								</Link>
+								<p className="text-md mt-4 font-semibold leading-tight text-gray-900">
+									₹{product.price}
+								</p>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
 		</>
 	);
 };
