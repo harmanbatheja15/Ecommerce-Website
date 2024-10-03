0d4f68c5388dcf9426c8d8481ea7be1145787898
harmanbatheja15 <harmanbatheja15@gmail.com>
2024-10-03 23:44:33 +0530

    added mobile nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,6 +8,12 @@ const Navbar = () => {
 	const token = localStorage.getItem('token');
 	const [user, setUser] = useState({});
 
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+	isMobileMenuOpen
+		? document.body.classList.add('overflow-hidden')
+		: document.body.classList.remove('overflow-hidden');
+
 	const navigate = useNavigate();
 
 	const getUser = async () => {
@@ -39,11 +45,11 @@ const Navbar = () => {
 					},
 				}
 			);
-
+			navigate('/');
 			// Remove isAuthenticated and token from localStorage
 			localStorage.removeItem('isAuthenticated');
 			localStorage.removeItem('token');
-			navigate('/');
+			setIsMobileMenuOpen(false);
 		} catch (error) {
 			console.log('Logout failed!');
 		}
@@ -52,10 +58,10 @@ const Navbar = () => {
 	return (
 		<>
 			<header className='relative w-full border-b bg-white pb-4'>
-				<div className='mx-auto flex max-w-7xl items-center justify-between px-4 py-2'>
+				<div className='mx-auto max-w-7xl flex items-center justify-between px-4 py-2'>
 					<div className='inline-flex items-center space-x-2'>
 						<span className='font-bold'>
-							<Link to='/'>Ecommerce</Link>
+							<Link to='/' onClick={() => setIsMobileMenuOpen(false)}>Ecommerce</Link>
 						</span>
 					</div>
 					<div className='hidden lg:block'>
@@ -131,8 +137,113 @@ const Navbar = () => {
 							</button>
 						</Link>
 					</div>
+					<div
+						className='lg:hidden'
+						onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+					>
+						<svg
+							width='16'
+							height='16'
+							viewBox='0 0 16 16'
+							fill='none'
+							xmlns='http://www.w3.org/2000/svg'
+						>
+							<path
+								d='M2 8H14M2 4H14M6 12H14'
+								stroke='black'
+								strokeWidth='1.33333'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+							/>
+						</svg>
+					</div>
 				</div>
 			</header>
+			{isMobileMenuOpen && (
+				<div className='lg:hidden absolute bg-white z-[100] h-full w-full'>
+					<ul className='flex flex-col space-y-4 items-center'>
+						<li>
+							<Link
+								to='/'
+								className='text-sm font-semibold text-gray-800 hover:text-gray-900'
+								onClick={() => setIsMobileMenuOpen(false)}
+							>
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link
+								to='/wishlist'
+								className='text-sm font-semibold text-gray-800 hover:text-gray-900'
+								onClick={() => setIsMobileMenuOpen(false)}
+							>
+								Wishlist
+							</Link>
+						</li>
+						<li>
+							<Link to='/cart' onClick={() => setIsMobileMenuOpen(false)}>
+								<button
+									type='button'
+									className='rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black'
+								>
+									Cart
+								</button>
+							</Link>
+						</li>
+						{isAuthenticated ? (
+							<>
+								{user?.admin && (
+									<li>
+										<Link
+											to='/addNewProduct'
+											className='text-sm font-semibold text-gray-800 hover:text-gray-900'
+											onClick={() => setIsMobileMenuOpen(false)}
+										>
+											Add Product
+										</Link>
+									</li>
+								)}
+								<li>
+									<Link
+										onClick={handleLogout}
+										className='text-sm font-semibold text-gray-800 hover:text-gray-900'
+									>
+										Logout
+									</Link>
+								</li>
+							</>
+						) : (
+							<>
+								<li>
+									<Link
+										to='/signup'
+										className='text-sm font-semibold text-gray-800 hover:text-gray-900'
+										onClick={() => setIsMobileMenuOpen(false)}
+									>
+										Signup
+									</Link>
+								</li>
+								<li>
+									<Link
+										to='/signin'
+										className='text-sm font-semibold text-gray-800 hover:text-gray-900'
+										onClick={() => setIsMobileMenuOpen(false)}
+									>
+										Signin
+									</Link>
+								</li>
+							</>
+						)}
+						{isAuthenticated && (
+							<li>
+								<span className='text-sm font-semibold text-gray-800 hover:text-gray-900 border p-2 mr-3'>
+									Welcome, {user?.name}
+								</span>
+							</li>
+						)}
+					</ul>
+				</div>
+			)}
 		</>
 	);
 };
--- frontend/src/pages/Wishlist.jsx
+++ frontend/src/pages/Wishlist.jsx
@@ -8,18 +8,16 @@ const Wishlist = () => {
 
 	const token = localStorage.getItem('token');
 	const navigate = useNavigate();
+	const isAuthenticated = localStorage.getItem('isAuthenticated');
 
 	useEffect(() => {
 		const fetchWishlist = async () => {
 			try {
-				const response = await axios.get(
-					`${API}/wishlist`,
-					{
-						headers: {
-							Authorization: `Bearer ${token}`,
-						},
-					}
-				);
+				const response = await axios.get(`${API}/wishlist`, {
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				});
 				const data = response.data;
 				setWishlist(data.productsInWishlist);
 			} catch (error) {
@@ -31,39 +29,47 @@ const Wishlist = () => {
 
 	return (
 		<>
-			<div className='mx-auto max-w-7xl px-2'>
-				<h1 className='text-3xl mt-9 mb-6'>Wishlist</h1>
-				{/* wishlist */}
-				<div className='grid gap-6 gap-y-10 py-6 md:grid-cols-2 lg:grid-cols-3'>
-					{wishlist?.length === 0 ? (
-						<div className=''>
-							No products in wishlist
-						</div>
-					) : (
-						wishlist?.map((product, index) => (
-							<div key={index} className='border'>
-								<Link to={`/product/${product._id}`}>
-									<img
-										src={product.imageUrl}
-										className='w-full h-96 rounded-md'
-										alt={product.title}
-									/>
-								</Link>
-								<div className='min-h-min p-3'>
+			{isAuthenticated ? (
+				<div className='mx-auto max-w-7xl px-2'>
+					<h1 className='text-3xl mt-9 mb-6'>Wishlist</h1>
+					<div className='grid gap-6 gap-y-10 py-6 md:grid-cols-2 lg:grid-cols-3'>
+						{wishlist?.length === 0 ? (
+							<div className=''>No products in wishlist</div>
+						) : (
+							wishlist?.map((product, index) => (
+								<div key={index} className='border'>
 									<Link to={`/product/${product._id}`}>
-										<p className='mt-4 flex-1 text-base font-semibold text-gray-900 underline'>
-											{product.title}
-										</p>
+										<img
+											src={product.imageUrl}
+											className='w-full h-96 rounded-md'
+											alt={product.title}
+										/>
 									</Link>
-									<p className='text-md mt-4 font-semibold leading-tight text-gray-900'>
-										₹{product.price}
-									</p>
+									<div className='min-h-min p-3'>
+										<Link to={`/product/${product._id}`}>
+											<p className='mt-4 flex-1 text-base font-semibold text-gray-900 underline'>
+												{product.title}
+											</p>
+										</Link>
+										<p className='text-md mt-4 font-semibold leading-tight text-gray-900'>
+											₹{product.price}
+										</p>
+									</div>
 								</div>
-							</div>
-						))
-					)}
+							))
+						)}
+					</div>
 				</div>
-			</div>
+			) : (
+				<div className="mx-auto max-w-7xl px-2 text-center">
+					<h1 className="text-2xl mt-9 mb-6">
+						Please{" "}
+						<Link to="/signin" className="underline text-blue-800">
+							Login
+						</Link>
+					</h1>
+				</div>
+			)}
 		</>
 	);
 };
