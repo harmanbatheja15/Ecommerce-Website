cd33e4aca1e999c8ca6f83899c624cb65a338cb6
Harman Batheja <harmanbatheja15@gmail.com>
2024-11-21 14:23:20 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 const JWT_SECRET = "harman_server";
-DB_CONNECTION="mongodb+srv://harman:harman123@cluster0.7wohrxa.mongodb.net/ecommerce-app"
+DB_CONNECTION="mongodb+srv://username:password@cluster0.7wohrxa.mongodb.net/ecommerce-app"
 
 module.exports = { JWT_SECRET, DB_CONNECTION };
